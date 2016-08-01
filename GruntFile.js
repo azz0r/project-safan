5dd05739ae873c19771074960bfd29f1c1fbd54a
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-08-01 22:21:21 +0100

    Grunt contrib tasks

--- GruntFile.js
+++ GruntFile.js
@@ -1,10 +1,5 @@
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-open');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  require('load-grunt-tasks')(grunt);
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     connect: {
